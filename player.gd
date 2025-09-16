1bafd34c92bad493f478f2db3ec39deb3ea10d85
Simon Döhl <simon.doehl@googlemail.com>
2025-09-16 16:21:26 +0200

    only planar movement

--- player.gd
+++ player.gd
@@ -2,10 +2,14 @@ extends RigidBody3D
 
 @onready var player_cam_arm: SpringArm3D = %PlayerCamArm
 
+var max_speed := 1000
+
 func _process(_delta: float) -> void:
     if Input.is_action_just_pressed("ui_accept"):
         apply_impulse(Vector3(0, 1, 0) * 300)
 
     var input_dir := Input.get_vector("a", "d", "w", "s")
-    var direction := (player_cam_arm.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-    apply_force(direction * 100)
+    var direction_xz := input_dir.rotated(-player_cam_arm.rotation.y)
+    var direction := Vector3(direction_xz.x, 0, direction_xz.y)
+    var forward_speed := (linear_velocity * direction).length()
+    apply_force(direction * (max_speed - forward_speed))
